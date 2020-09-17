379dcf03e50d6ae29778d97d5430f9f15495e992
boom100100 <bsd231@nyu.edu>
2020-09-17 15:00:00 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def largest_palindrome_product
+  i, a, b = 0, 999, 999
+  while a >= 100 && b >= 100
+    result = a * b
+    result_string = result.to_s
+    return result if result_string == result_string.reverse
+    i += 1
+    if i < 100
+      a -= 1
+    else
+      i = 0
+      b -= 1
+      a = b
+    end
+  end
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+  def initialize
+  end
+
+  def answer
+    i, a, b = 0, 999, 999
+    while a >= 100 && b >= 100
+      result = a * b
+      result_string = result.to_s
+      return result if result_string == result_string.reverse
+      i += 1
+      if i < 100
+        a -= 1
+      else
+        i = 0
+        b -= 1
+        a = b
+      end
+    end
+  end
+end
