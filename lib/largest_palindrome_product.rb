81cc772f62f29e21e1bc5f587a788f18a7871ef6
trishogen <patriciaogen@gmail.com>
2020-09-26 22:02:45 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,19 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  palindrome = -1
+
+  for num1 in (100..999)
+    for num2 in (100..999)
+      product = num1 * num2
+
+      if is_palindrome?(product) && product > palindrome
+        palindrome = product
+      end
+    end
+  end
+
+  palindrome
+end
+
+def is_palindrome?(input)
+  input.to_s == input.to_s.reverse
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,22 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def answer
+    palindrome = -1
+
+    for num1 in (100..999)
+      for num2 in (100..999)
+        product = num1 * num2
+
+        if is_palindrome?(product) && product > palindrome
+          palindrome = product
+        end
+      end
+    end
+
+    palindrome
+  end
+
+  def is_palindrome?(input)
+    input.to_s == input.to_s.reverse
+  end
+end
