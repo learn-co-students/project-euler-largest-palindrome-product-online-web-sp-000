ee54910ed50bff06673adf655be13f0f633f58db
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-10 21:57:45 +0100

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,29 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    def answer
+        first_counter = 999
+        second_counter = 999
+        result = 0
+    
+        until second_counter === 100
+            str = (first_counter * second_counter).to_s
+            first_half = str[0..((str.size-1)/2.floor)]
+            second_half = str[first_half.size..str.size-1].split("").reverse.join("")
+    
+            if result < (second_counter * first_counter)
+                if first_half === second_half || first_half[0..first_half.size-2] === second_half
+                    result = first_counter * second_counter
+                end
+            end 
+    
+            first_counter -= 1
+            
+            if first_counter === 99
+                second_counter -= 1
+                first_counter = 999
+            end
+        end
+    
+        result
+    end
+end
